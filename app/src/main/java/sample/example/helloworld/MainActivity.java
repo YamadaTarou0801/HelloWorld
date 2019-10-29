e8aea7debc8243c67a6b5e61dcb31b244bfa9a47
yamada <yamaguchi20191026@gmail.com>
2019-10-29 06:43:51 -0700

    log comit

--- app/src/main/java/sample/example/helloworld/MainActivity.java
+++ app/src/main/java/sample/example/helloworld/MainActivity.java
@@ -1,16 +1,22 @@
 package sample.example.helloworld;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "test dayo-n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"HelloWorld",Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG,"LOG TEST");
+        Log.d(TAG,"LOG TEST2");
+
     }
 }
