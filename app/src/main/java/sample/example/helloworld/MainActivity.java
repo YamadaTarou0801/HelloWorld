46dd317a2a5ac2b5fbe914920d65d62e0a6394d4
yamada <yamaguchi20191026@gmail.com>
2019-10-29 05:20:18 -0700

    Toastオブジェクトの生成をして、Helloが出るようにした。

--- app/src/main/java/sample/example/helloworld/MainActivity.java
+++ app/src/main/java/sample/example/helloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package sample.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"HelloWorld",Toast.LENGTH_SHORT).show();
     }
 }
